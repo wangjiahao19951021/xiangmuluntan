c110dc17f6d1a01be046b7337e8d1cbbcc4d0a80
wangjiahao <196279741@qq.com>
2019-01-29 00:24:53 +0800

    1

--- routes/handers/remove_commits_handler.js
+++ routes/handers/remove_commits_handler.js
@@ -1,7 +1,26 @@
 const connect = require("../../modules/connect")
 
-const removecommitsHandler = {
+// 也需要引入 ObjectId
+const ObjectId = require("mongodb").ObjectId
 
+const removecommitsHandler = (req, res) => {
+    let {id} = req.body
+    // 定义返回的内容
+    let content = {err: null, data: {}}
+    connect((db, close) => {
+        let commits = db.collection('commits');
+        commits.remove({_id: ObjectId(id)}, (err, results) => {
+            if (err) {
+                console.log(err)
+                content.err = 500
+                return false
+            }
+            content.err = 200
+            res.send(content);
+            close()
+
+        })
+    })
 }
 
 module.exports = removecommitsHandler
