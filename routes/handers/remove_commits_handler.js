6f4c1049d2c6694a2a19fe1c229b770dce243f5f
wangjiahao <196279741@qq.com>
2019-02-26 00:09:09 +0800

    1

--- routes/handers/remove_commits_handler.js
+++ routes/handers/remove_commits_handler.js
@@ -5,6 +5,9 @@ const ObjectId = require("mongodb").ObjectId
 
 const removecommitsHandler = (req, res) => {
     let {id} = req.body
+
+    console.log(id)
+    console.log(ObjectId(id))
     // 定义返回的内容
     let content = {err: null, data: {}}
     connect((db, close) => {
@@ -19,6 +22,18 @@ const removecommitsHandler = (req, res) => {
             res.send(content);
             close()
 
+        })
+    })
+
+    // 删除点赞的数据库 
+    connect((db, close) => {
+        let commits_hot = db.collection("like");
+        commits_hot.remove({id: id}, (err, results) => {
+            if (err) {
+                console.log(err)
+                return false
+            }
+            close()
         })
     })
 }
